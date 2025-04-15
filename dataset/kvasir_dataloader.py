6764bdb5c47e4282f1d6659eb80ff40d01911552
Atharv Shingewar <58426349+Atharv157@users.noreply.github.com>
2025-04-15 19:20:59 +0530

    Update kvasir_dataloader.py

--- dataset/kvasir_dataloader.py
+++ dataset/kvasir_dataloader.py
@@ -37,15 +37,15 @@ class KvasirSegDataset(Dataset):
         return {"sat_img": image, "map_img": mask}
 
 
-def get_datasets(image_dir, mask_dir, val_size=0.2, seed=42):
+def get_datasets(image_dir, mask_dir, val_size=0.2, seed=42, image_size=256):
     all_images = sorted(os.listdir(image_dir))
     train_imgs, val_imgs = train_test_split(all_images, test_size=val_size, random_state=seed)
 
     train_dataset = KvasirSegDataset(
-        image_dir, mask_dir, image_list=train_imgs, transform=get_transforms(train=True)
+        image_dir, mask_dir, image_list=train_imgs, transform=get_transforms(train=True, image_size=image_size)
     )
     val_dataset = KvasirSegDataset(
-        image_dir, mask_dir, image_list=val_imgs, transform=get_transforms(train=False)
+        image_dir, mask_dir, image_list=val_imgs, transform=get_transforms(train=False, image_size=image_size)
     )
     return train_dataset, val_dataset
 
@@ -67,7 +67,7 @@ def get_transforms(train=True, image_size=256):
         ])
     else:
         return A.Compose([
-            A.Resize(256, 256),
+            A.Resize(image_size, image_size),
             A.Normalize(mean=(0.485, 0.456, 0.406),
                         std=(0.229, 0.224, 0.225)),
             ToTensorV2()
