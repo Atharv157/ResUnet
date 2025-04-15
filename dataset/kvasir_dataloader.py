d1147e614dcc5e60e16e611979ad29b19a77b47f
Atharv Shingewar <58426349+Atharv157@users.noreply.github.com>
2025-04-15 18:59:16 +0530

    Create kvasir_dataloader.py

---
+++ dataset/kvasir_dataloader.py
@@ -0,0 +1,74 @@
+import os
+import cv2
+import numpy as np
+from torch.utils.data import Dataset
+from sklearn.model_selection import train_test_split
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+
+class KvasirSegDataset(Dataset):
+    def __init__(self, image_dir, mask_dir, image_list=None, transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+
+        self.images = sorted(image_list) if image_list is not None else sorted(os.listdir(image_dir))
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_name = self.images[idx]
+        img_path = os.path.join(self.image_dir, img_name)
+        mask_path = os.path.join(self.mask_dir, img_name)
+
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+
+        mask = (mask > 127).astype(np.float32)
+
+        if self.transform:
+            augmented = self.transform(image=image, mask=mask)
+            image = augmented['image']
+            mask = augmented['mask']
+
+        return {"sat_img": image, "map_img": mask}
+
+
+def get_datasets(image_dir, mask_dir, val_size=0.2, seed=42):
+    all_images = sorted(os.listdir(image_dir))
+    train_imgs, val_imgs = train_test_split(all_images, test_size=val_size, random_state=seed)
+
+    train_dataset = KvasirSegDataset(
+        image_dir, mask_dir, image_list=train_imgs, transform=get_transforms(train=True)
+    )
+    val_dataset = KvasirSegDataset(
+        image_dir, mask_dir, image_list=val_imgs, transform=get_transforms(train=False)
+    )
+    return train_dataset, val_dataset
+
+
+def get_transforms(train=True):
+    if train:
+        return A.Compose([
+            A.Resize(256, 256),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.2),
+            A.RandomRotate90(p=0.5),
+            A.ShiftScaleRotate(
+                shift_limit=0.0625, scale_limit=0.1, rotate_limit=15, p=0.5
+            ),
+            A.RandomBrightnessContrast(p=0.2),
+            A.Normalize(mean=(0.485, 0.456, 0.406),
+                        std=(0.229, 0.224, 0.225)),
+            ToTensorV2()
+        ])
+    else:
+        return A.Compose([
+            A.Resize(256, 256),
+            A.Normalize(mean=(0.485, 0.456, 0.406),
+                        std=(0.229, 0.224, 0.225)),
+            ToTensorV2()
+        ])
